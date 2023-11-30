50c699a5f0a9d2d3814cf5a7fe699e523a2a4156
katanainsidee <whoyuster@gmail.com>
2023-11-30 23:51:50 +0300

    добавление формы

--- events/urls.py
+++ events/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('', views.events, name='events'),
-    path('events/<int:event_id>/', views.event_detail, name='event_detail'),
+    path('<int:event_id>/', views.event_detail, name='event_detail'),
 ]
