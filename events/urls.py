380fd23e9f1ea3046a44c14bf399915eb69bff47
katanainsidee <whoyuster@gmail.com>
2023-11-30 21:56:55 +0300

    добавление шапки со стилями для вывода всех мероприятий, добавление формы (пустой) для вывода каждого мероприятия при нажатии на кнопку открыть

--- events/urls.py
+++ events/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 urlpatterns = [
     path('', views.events, name='events'),
+    path('events/<int:event_id>/', views.event_detail, name='event_detail'),
 ]
--- events/views.py
+++ events/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Events
 
 
 def events(request):
     current_events = Events.objects.order_by('-date')
     return render(request, 'events/events.html', {'current_events': current_events})
+
+
+def event_detail(request, event_id):
+    event = get_object_or_404(Events, pk=event_id)
+    return render(request, 'events/event_detail.html', {'event': event})
